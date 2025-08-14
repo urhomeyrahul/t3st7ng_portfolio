1090a75d699146a00e690eeecd522a41bb97efa3
RahulAggarwal <RahulAggarwal@GGINITLPGY1YWL3>
2025-08-14 16:46:09 +0530

    Added-Technology-Stack

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,32 @@ import { AnimatedThemeToggler } from "@/components/magicui/animated-theme-toggle
 import { ScrollProgress } from "@/components/magicui/scroll-progress";
 import Particles from '../blocks/Backgrounds/Particles/Particles';
 import { SmoothCursor } from "../components/ui/smooth-cursor";
+import { ScrollVelocityContainer, ScrollVelocityRow, } from "@/components/magicui/scroll-based-velocity";
 
 export default function Home() {
 
   const [scroll, setScrolled] = useState(false);
+
+  const IMAGES_ROW_A = [
+    { label: "JavaScript", src: "https://cdn.simpleicons.org/javascript?viewbox=auto&size=20" },
+    { label: "React", src: "https://cdn.simpleicons.org/react?viewbox=auto&size=20" },
+    { label: "TypeScript", src: "https://cdn.simpleicons.org/typescript?viewbox=auto&size=20" },
+    { label: "Next.js", src: "https://cdn.simpleicons.org/nextdotjs?viewbox=auto&size=20" },
+    { label: "Tailwind CSS", src: "https://cdn.simpleicons.org/tailwindcss?viewbox=auto&size=20" },
+    { label: "Vue.js", src: "https://cdn.simpleicons.org/vuedotjs?viewbox=auto&size=20" },
+    { label: "Android Studio", src: "https://cdn.simpleicons.org/androidstudio?viewbox=auto&size=20" },
+    { label: "Nuxt.js", src: "https://cdn.simpleicons.org/nuxt?viewbox=auto&size=20" },
+    { label: "CSS", src: "https://cdn.simpleicons.org/css?viewbox=auto&size=20" },
+  ];
+
+  const IMAGES_ROW_B = [
+    { label: "GitHub", src: "https://cdn.simpleicons.org/github?viewbox=auto&size=20" },
+    { label: "GitHub Copilot", src: "https://cdn.simpleicons.org/githubcopilot?viewbox=auto&size=20" },
+    { label: "Postman", src: "https://cdn.simpleicons.org/postman?viewbox=auto&size=20" },
+    { label: "Docker", src: "https://cdn.simpleicons.org/docker?viewbox=auto&size=20" },
+    { label: "Git", src: "https://cdn.simpleicons.org/git?viewbox=auto&size=20" },
+    { label: "Figma", src: "https://cdn.simpleicons.org/figma?viewbox=auto&size=20" },
+  ];
 
   useEffect(() => {
     const handleScroll = () => {
@@ -62,9 +84,9 @@ export default function Home() {
             className="absolute inset-0 z-0"
           />
 
-          <div className="absolute inset-0 flex flex-col justify-center px-5 z-10 pointer-events-none">
+          <div className="absolute inset-0 flex flex-col justify-center px-15 z-10 pointer-events-none">
             <h1
-              className="font-bold text-5xl flex px-6"
+              className="font-bold text-5xl flex px-6 py-2"
             >
               The Programmer
             </h1>
@@ -72,11 +94,64 @@ export default function Home() {
               Front-End Developer crafting high-performance, responsive, and user-friendly web
               <br />applications using modern technologies
             </p>
+
+
           </div>
 
         </div>
+        <div className="py-20">
+          <h1 className="font-bold text-5xl flex px-22 py-2">
+            Technology Stack
+          </h1>
+        </div>
+
+        <div className="relative flex w-full flex-col items-center justify-center overflow-x-hidden">
+          <div className="relative w-[80%] rounded-3xl overflow-hidden">
+            <ScrollVelocityContainer>
+              <ScrollVelocityRow baseVelocity={4} direction={1} className="py-2">
+                {IMAGES_ROW_A.map((item, idx) => (
+                  <div key={idx} className="flex flex-col items-center mx-6">
+                    <img
+                      src={`${item.src}&ixlib=rb-4.0.3`}
+                      alt={item.label}
+                      width={240}
+                      height={160}
+                      loading="lazy"
+                      decoding="async"
+                      className="h-15 w-20 rounded-md"
+                    />
+                    <span className="mt-1 text-sm text-center">{item.label}</span>
+                  </div>
+                ))}
+              </ScrollVelocityRow>
+
+              <ScrollVelocityRow baseVelocity={6} direction={-1} className="py-4">
+                {IMAGES_ROW_B.map((item, idx) => (
+                  <div key={idx} className="flex flex-col items-center mx-6 my-6">
+                    <img
+                      src={`${item.src}&ixlib=rb-4.0.3`}
+                      alt={item.label}
+                      width={240}
+                      height={160}
+                      loading="lazy"
+                      decoding="async"
+                      className="h-15 w-26 rounded-lg"
+                    />
+                    <span className="mt-1 text-sm text-center">{item.label}</span>
+                  </div>
+                ))}
+              </ScrollVelocityRow>
+            </ScrollVelocityContainer>
+
+            <div className="pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-background"></div>
+            <div className="pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l from-background"></div>
+          </div>
+        </div>
+
 
       </div>
+
+
 
     </div>
   );
